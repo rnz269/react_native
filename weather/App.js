31d2a1808c2a6ca3014d84d6463630406e058ac0
Rahul Nallappa <rahul.nallappa@gmail.com>
2020-04-01 16:50:05 -0400

    added assets and utils

--- weather/App.js
+++ weather/App.js
@@ -1,5 +1,7 @@
 import React from 'react';
-import { StyleSheet, Text, KeyboardAvoidingView, Platform, TextInput} from 'react-native';
+import { StyleSheet, Text, KeyboardAvoidingView, Platform} from 'react-native';
+
+import SearchInput from "./components/SearchInput"
 
 export default function App() {
   return (
@@ -7,13 +9,7 @@ export default function App() {
       <Text style={[styles.largeText, styles.textStyle]}>San Francisco</Text>
       <Text style={[styles.smallText, styles.textStyle]}>Light Cloud</Text>
       <Text style={[styles.largeText, styles.textStyle]}>13°</Text>
-      <TextInput
-        autoCorrect={false}
-        placeholder="Search any city"
-        placeholderTextColor="white"
-        style={styles.textInput}
-        clearButtonMode="always"
-      />
+      <SearchInput placeholder="Search any city"/>
     </KeyboardAvoidingView>
   );
 }
@@ -34,16 +30,6 @@ const styles = StyleSheet.create({
     fontSize: 44,
   },
   smallText: {
-    fontSize: 18
-  },
-  textInput: {
-    backgroundColor: '#666',
-    color: 'white',
-    height: 40,
-    width: 300,
-    marginTop: 20,
-    marginHorizontal: 20,
-    paddingHorizontal: 10,
-    alignSelf: 'center'
+    fontSize: 18,
   }
 });

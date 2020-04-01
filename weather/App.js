c4627e6fd8bb2f33e168e153dc75fa8fbf151a4e
Rahul Nallappa <rahul.nallappa@gmail.com>
2020-04-01 16:08:08 -0400

    built in native components to build ui

--- weather/App.js
+++ weather/App.js
@@ -1,11 +1,20 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, KeyboardAvoidingView, Platform, TextInput} from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
+    <KeyboardAvoidingView style={styles.container} behavior="height">
+      <Text style={[styles.largeText, styles.textStyle]}>San Francisco</Text>
+      <Text style={[styles.smallText, styles.textStyle]}>Light Cloud</Text>
+      <Text style={[styles.largeText, styles.textStyle]}>13°</Text>
+      <TextInput
+        autoCorrect={false}
+        placeholder="Search any city"
+        placeholderTextColor="white"
+        style={styles.textInput}
+        clearButtonMode="always"
+      />
+    </KeyboardAvoidingView>
   );
 }
 
@@ -16,4 +25,25 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  textStyle: {
+    textAlign: 'center',
+    fontFamily:
+      Platform.OS === 'ios' ? 'AvenirNext-Regular' : 'Roboto',
+  },
+  largeText: {
+    fontSize: 44,
+  },
+  smallText: {
+    fontSize: 18
+  },
+  textInput: {
+    backgroundColor: '#666',
+    color: 'white',
+    height: 40,
+    width: 300,
+    marginTop: 20,
+    marginHorizontal: 20,
+    paddingHorizontal: 10,
+    alignSelf: 'center'
+  }
 });

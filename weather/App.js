b2014100abdc69597bc14b36544925d9c95b8ce2
Rahul Nallappa <rahul.nallappa@gmail.com>
2020-04-02 18:13:35 -0400

    connected to metaweather api, now fetching and displaying data conditionally

--- weather/App.js
+++ weather/App.js
@@ -1,34 +1,97 @@
 import React, {useState, useEffect} from 'react';
-import { StyleSheet, ImageBackground, Text, View, KeyboardAvoidingView, Platform } from 'react-native';
+import { StyleSheet, ImageBackground, Text, View, KeyboardAvoidingView, Platform, ActivityIndicator, StatusBar } from 'react-native';
 
 import SearchInput from "./components/SearchInput"
 import getImageForWeather from './utils/getImageForWeather'
 
+import {fetchLocationId, fetchWeather } from "./utils/api"
+
 
 export default function App() {
-  const [location, setLocation] = useState('')
-  const handleUpdateLocation = (text) => setLocation(text)
+  const [loading, setLoading] = useState(false)
+  const [error, setError] = useState(false)
+  const [location, setLocation] = useState('')  
+  const [temperature, setTemperature] = useState(0)
+  const [weather, setWeather] = useState('')
+  const [userInput, setUserInput] = useState('')
 
+// function to update the city upon mount
   useEffect(()=> {
-    handleUpdateLocation('San Francisco')
+    handleUpdateInput('San Francisco')
   }, []) 
+
+// update user input (temporarily city before checking validity)
+  const handleUpdateInput = input => {
+    if (!input) return
+    setUserInput(input)
+  }  
+
+// once user input has updated, setLoading
+  useEffect(()=> {
+    setLoading(true)
+  }, [userInput])
+
+// once loading has been updated (ensure updated to true), run callback
+// we need userInput to have updated (to use it like an argument here) and loading updated to ensure
+// action is taken when loading = true (which if condition checks, but without useEffect would return if false)
+  useEffect(()=> {
+      async function tapAPI() {
+        if (loading) {
+          try {
+            const locationId = await fetchLocationId(userInput)
+            const {location, weather, temperature} = await fetchWeather(locationId)
+            setLoading(false)
+            setError(false)
+            setLocation(location)
+            setWeather(weather)
+            setTemperature(temperature)
+          } catch (e) {
+            setLoading(false)
+            setError(true)
+          }
+        }
+      }
+    tapAPI()
+  }, [loading])
 
   return (
     <KeyboardAvoidingView style={styles.container} behavior="height">
+      <StatusBar barStyle="light-content" />
       <ImageBackground 
-        source={getImageForWeather('Clear')} 
+        source={getImageForWeather(weather)} 
         style={styles.imageContainer} 
         imageStyle={styles.image}
       >
         <View style={styles.detailsContainer}>
-          <Text style={[styles.largeText, styles.textStyle]}>{location}</Text>
-          <Text style={[styles.smallText, styles.textStyle]}>Light Cloud</Text>
-          <Text style={[styles.largeText, styles.textStyle]}>13°</Text>
-          <SearchInput onSubmit={handleUpdateLocation} placeholder="Search any city" />
-        </View>
+          <ActivityIndicator animating={loading} color="white" size="large" />
+          {!loading && (
+            <View>
+            {error && (
+                <Text style={[styles.smallText, styles.textStyle]}>
+                  Could not load weather, please try a different city.
+                </Text>
+              )}
+
+              {!error && (
+              <View>
+                <Text style={[styles.largeText, styles.textStyle]}>
+                  {location}
+                </Text>
+                <Text style={[styles.smallText, styles.textStyle]}>
+                  {weather}
+                </Text>
+                <Text style={[styles.largeText, styles.textStyle]}>
+                  {`${Math.round(temperature)}°`}
+                </Text>
+              </View>
+            )}
+              <SearchInput placeholder="Search any city" onSubmit={handleUpdateInput}/>
+            </View>
+          )}
+          </View>
       </ImageBackground>
     </KeyboardAvoidingView>
-  );
+  )
 }
 
 const styles = StyleSheet.create({

1854c417639b8c0d4f94930c4ef3092a366035fd
Rahul Nallappa <rahul.nallappa@gmail.com>
2020-05-29 11:49:24 -0400

    weather updates to comments

--- weather/App.js
+++ weather/App.js
@@ -122,6 +122,7 @@ const styles = StyleSheet.create({
   detailsContainer: {
     flex: 1,
     justifyContent: 'center',
+    alignItems: 'center',
     backgroundColor: 'rgba(0,0,0,0.2)'
   }
 });

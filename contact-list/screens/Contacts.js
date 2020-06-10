b8a4296a6355bdbc407e46ce13cdadfb9bed6dbb
Rahul Nallappa <rahul.nallappa@gmail.com>
2020-06-10 15:24:02 -0400

    Contacts screen setup

--- contact-list/screens/Contacts.js
+++ contact-list/screens/Contacts.js
@@ -5,37 +5,91 @@ import ContactListItem from '../components/ContactListItem'
 import {fetchContacts} from '../utils/api'
 
 export default function Contacts() {
-// fetchContacts returns an array of objects
-// each object is a contact w/ props:
-// id, name, avatar, phone, cell, email, favorite 
-// we must map this objects array to ContactListItem array 
-// supply name, avatar, phone, onPress to each CLI
+	// fetchContacts returns an array of objects
+	// each object is a contact w/ props:
+	// id, name, avatar, phone, cell, email, favorite 
+	// we must map this objects array to ContactListItem array 
+	// we perform mapping using FlatList
 
-// setup state to capture ContactListItem data array
-const [data, setData] = useState([]) 
+	// setup state to capture ContactListItem data array
+	const [data, setData] = useState([])
+	const [loading, setLoading] = useState(true) 
+	const [error, setError] = useState(false)
 
-// on component mount
-useEffect(()=> {
-	async function fetchData() {
-		const data = await fetchContacts()
-		setData(data)
+	// on component mount
+	useEffect(()=> {
+		async function fetchData() {
+			try {
+				const data = await fetchContacts()
+				setData(data)
+				setLoading(false)
+			}
+			catch (e) {
+				setError(true)
+				setLoading(false)
+			}
+		}
+		fetchData()
+	}, [])
+
+	// sort data alphabetically
+	const dataSorted = data.sort((a,b) => (
+		a.name.localeCompare(b.name)
+	))
+
+	const keyExtractor = ({id}) => id.toString()
+	const renderItem = ({item}) => {
+		return (
+			<ContactListItem 
+				name={item.name}
+				avatar={item.avatar}
+				phone={item.phone}
+				onPress={()=>{}}
+			/>
+		)
 	}
-	fetchData()
-}, [])
 
-useEffect(()=> {
-	console.log(data)
-}, [data])
+/**************** RETURNING UI ********************/
+	
+	// if loading, return spinner
+	if (loading) {
+		return (
+			<View style={styles.container}>
+				<ActivityIndicator size={'large'} animating={loading}/>
+			</View>
+		)
+	}
 
-return (
-	<View>
-		<Text>Hello from Contacts.js</Text>
-	</View>
-)
+	// if error, print error message
+	if (error) {
+		return (
+			<View style={styles.container}>
+				<Text style={styles.text}>There was an error retrieving data</Text>
+			</View>
+			)
+	}
+
+	// otherwise render flatlist
+	return (
+		<View style={styles.container}>
+			<FlatList
+				data={dataSorted}
+				renderItem={renderItem}
+				keyExtractor={keyExtractor}
+			/>
+		</View>
+	)
 
 	
 }
 
 const styles = StyleSheet.create({
-
+	container: {
+		flex: 1,
+		justifyContent: 'center',
+		backgroundColor: 'white',
+	},
+	text: {
+		textAlign: 'center',
+	}
 })

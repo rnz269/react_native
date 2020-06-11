c32aa756a9676f5bfc29c585e27d1de7f005035c
Rahul Nallappa <rahul.nallappa@gmail.com>
2020-06-11 15:21:52 -0400

    added User screen to contacts app

---
+++ contact-list/screens/User.js
@@ -0,0 +1,73 @@
+import React, {useState, useEffect} from 'react'
+import {StyleSheet, ActivityIndicator, View, Text} from 'react-native'
+
+import ContactThumbnail from '../components/ContactThumbnail'
+
+import {fetchUserContact} from '../utils/api'
+import colors from '../utils/colors'
+
+export default function User() {
+
+	const [user, setUser] = useState()
+	const [loading, setLoading] = useState(true)
+	const [error, setError] = useState(false)
+
+	useEffect(()=> {
+		async function getUserContact() {
+			try {
+				const userLoggedIn = await fetchUserContact()
+				setUser(userLoggedIn)
+				setLoading(false)
+			} catch (e) {
+				setError(true)
+				setLoading(false)
+			}
+		}
+		getUserContact()
+	}, [])
+
+	if (loading) {
+		return (
+			<View style={styles.container}>
+				<ActivityIndicator size={'large'} animating={loading} />
+			</View>
+		)
+	}
+
+	if (error) {
+		return (
+			<View style={styles.container}>
+				<Text style={styles.text}>There was an error fetching data</Text>
+			</View>
+		)
+	}
+
+	return (
+		<View style={styles.container}>
+			<ContactThumbnail
+				name={user.name}
+				phone={user.phone}
+				avatar={user.avatar}
+			 />
+		</View>
+	)
+
+}
+
+User.navigationOptions = {
+	title: 'Me',
+	headerStyle: {backgroundColor: colors.blue},
+	headerTintColor: 'white',
+}
+
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		justifyContent: 'center',
+		alignItems: 'center',
+		backgroundColor: colors.blue,
+	},
+	text: {
+		textAlign: 'center',
+	}
+})

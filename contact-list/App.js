bcf8fc9e60247ebe0d761ee8bbe1c1b80c19acee
Rahul Nallappa <rahul.nallappa@gmail.com>
2020-07-12 11:20:04 -0400

    testing nested git with expo command

--- contact-list/App.js
+++ contact-list/App.js
@@ -1,15 +1,21 @@
-import React from 'react'
-import {Provider} from 'react-redux'
-import store from './redux'
-
-import AppContainer from './routes'
-
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
-  	<Provider store={store}>
-  		<AppContainer />
-  	</Provider>
-  	)
+    <View style={styles.container}>
+      <Text>Open up App.js to start working on your app!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
 }
 
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
